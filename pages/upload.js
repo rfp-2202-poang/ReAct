ab19d1a4205320f0d3c0e65190cffffadbecb569
Charlie <charliebailey24@gmail.com>
2022-05-06 14:36:53 -0700

    Setting up the upload page

---
+++ pages/upload.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css'
+
+export default function Upload() {
+
+
+
+  return (
+    <div className={styles.container}>
+      <title>Upload</title>
+      <div>this is the upload page</div>
+      <button>Upload</button>
+      <button>Record</button>
+    </div>
+  )
+}
