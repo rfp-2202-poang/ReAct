1c00c4fe6a5d889885a8ed71d43eb2cd9c193eb4
Sabas Segovia <sasegovia@ucdavis.edu>
2022-05-06 19:12:17 -0700

    feat: finished helpers.js, still need to connect to front

--- server/SpeechToText.js
+++ server/SpeechToText.js
@@ -7,7 +7,7 @@ const { IamAuthenticator } = require("ibm-watson/auth");
 const fs = require("fs");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 //connection
 const speechToText = new SpeechToTextV1({
@@ -20,53 +20,57 @@ const speechToText = new SpeechToTextV1({
 ////////////////////////////
 ////LIST LANGUAGES AVAILABLE
 ////////////////////////////
-speechToText
-  .listModels()
-  .then((speechModels) => {
-    // console.log(JSON.stringify(speechModels, null, 2));
-    console.log(speechModels.result);
-  })
-  .catch((err) => {
-    console.log("error:", err);
-  });
-
-/////////////////
-///GET A LANGUAGE
-/////////////////
-const getModelParams = {
-  modelId: "en-US_BroadbandModel",
-};
-
-speechToText
-  .getModel(getModelParams)
-  .then((speechModel) => {
-    // console.log(JSON.stringify(speechModel, null, 2));
-    console.log(speechModel.result);
-  })
-  .catch((err) => {
-    console.log("error:", err);
-  });
-
-//////////////////////////////
-// CONVERTS AUDIO FILE TO TEXT
-//////////////////////////////
-// const recognizeParams = {
-//   //location of file
-//   audio: fs.createReadStream("audio-file2.flac"),
-//   contentType: "audio/flac",
-//   wordAlternativesThreshold: 0.9,
-//   keywords: ["colorado", "tornado", "tornadoes"],
-//   keywordsThreshold: 0.5,
-// };
-
 // speechToText
-//   .recognize(recognizeParams)
-//   .then((speechRecognitionResults) => {
-//     console.log(speechRecognitionResults.result.results);
-//     // console.log(JSON.stringify(speechRecognitionResults, null, 2));
+//   .listModels()
+//   .then((speechModels) => {
+//     // console.log(JSON.stringify(speechModels, null, 2));
+//     console.log(speechModels.result);
 //   })
 //   .catch((err) => {
 //     console.log("error:", err);
 //   });
+
+// /////////////////
+// ///GET A LANGUAGE
+// /////////////////
+// const getModelParams = {
+//   modelId: "en-US_BroadbandModel",
+// };
+
+// speechToText
+//   .getModel(getModelParams)
+//   .then((speechModel) => {
+//     // console.log(JSON.stringify(speechModel, null, 2));
+//     console.log(speechModel.result);
+//   })
+//   .catch((err) => {
+//     console.log("error:", err);
+//   });
+
+// ////////////////////////////
+// CONVERTS AUDIO FILE TO TEXT
+// ////////////////////////////
+
+const recognizeParams = {
+  //location of file
+  audio: fs.createReadStream("server/audio-file2-1.flac"),
+  contentType: "audio/flac",
+  wordAlternativesThreshold: 0.9,
+  keywords: ["colorado", "tornado", "tornadoes"],
+  keywordsThreshold: 0.5,
+};
+
+speechToText
+  .recognize(recognizeParams)
+  //.then get string transcript
+  .then((speechRecognitionResults) => {
+    console.log(
+      speechRecognitionResults.result.results[0].alternatives[0].transcript
+    );
+    // console.log(JSON.stringify(speechRecognitionResults, null, 2));
+  })
+  .catch((err) => {
+    console.log("error:", err);
+  });
 
 app.listen(3002);
