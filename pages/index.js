cbffd840cc9d461e312acf0a320e42de82e82942
JoeVanCamp <vancampjoe@gmail.com>
2022-05-06 22:26:46 -0700

    Lock 3D controls, added temporary title

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
-
+import gsap from 'gsap';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 
@@ -19,30 +19,31 @@ export default function Home() {
     // Get window sizes
     renderer.setSize(window.innerWidth, window.innerHeight);
     // renderer.setClearColor(0x7ec0ee, 1);
+
+    // LIGHTING
     const light = new THREE.DirectionalLight(0xffffff, 5);
     const spotLight = new THREE.SpotLight(0xffffff, 3);
-
     light.position.set(1, 1, 10)
     spotLight.position.set(0, 4, 0)
     scene.add(spotLight);
     scene.add(light);
 
+    // ENVIRONMENT
     const env = new RGBELoader().load('models/studio_small_08_1k.hdr');
     scene.environment = env;
 
-    // Target correct div
-    mountRef.current.appendChild(renderer.domElement);
 
     // Assign Ref property for later cleanup
     const cleanup = mountRef.current;
-    const controls = new OrbitControls(camera, renderer.domElement);
 
+
+    // FLOOR TEXTURE
     let texture = new THREE.TextureLoader().load('models/floor_tiles_08_diff_1k.jpg');
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(12, 12);
 
+    // FLOOR MESH
     let material = new THREE.MeshPhysicalMaterial({ map: texture, bumpMap: texture });
-
     let geometry = new THREE.PlaneBufferGeometry(100, 100);
     let ground = new THREE.Mesh(geometry, material);
     ground.rotation.z = Math.PI / 180 * -45;
@@ -50,30 +51,47 @@ export default function Home() {
     ground.position.y = -1.3;
     scene.add(ground);
 
-
+    // LOAD OBJECT
     const loader = new GLTFLoader();
     loader.load('models/del.glb', (gltf) => {
       let model = gltf.scene;
       scene.add(model);
     })
 
+
+    // CAMERA CONTROLS
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false
+    controls.enablePan = false
+    controls.enableRotate = false
     camera.position.z = 4;
     controls.minPolarAngle = Math.PI / 2;
     controls.maxPolarAngle = Math.PI / 2;
     controls.autoRotate = true;
+
+    // Target correct div
+    mountRef.current.appendChild(renderer.domElement);
+
     const animate = () => {
       controls.update();
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
     }
     animate();
+
+
     // Remove scene on unmount
     return () => cleanup.removeChild(renderer.domElement);
   }, []);
 
   return (
     <div ref={mountRef}>
-      <h2>HELLO WORLD</h2>
+      <div>
+        <h1 className="title">
+          Script.ly
+        </h1>
+      </div>
+
     </div>
   )
 }
