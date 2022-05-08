9bccdfccf02756fe313036a5eede85fe3499d62b
Sabas Segovia <sasegovia@ucdavis.edu>
2022-05-08 14:45:00 -0700

    feat: push to get new updates

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import * as THREE from "three";
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
-import gsap from 'gsap';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import gsap from "gsap";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // import styles from '../styles/Home.module.css';
 
-
-
 export default function Home() {
-
   const mountRef = useRef(null);
 
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(70, window.innerHeight / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerHeight / window.innerHeight,
+      0.1,
+      1000
+    );
     const renderer = new THREE.WebGLRenderer({ antialias: true });
 
     // These do something important I think
@@ -28,35 +30,40 @@ export default function Home() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     // renderer.setClearColor(0x7ec0ee, 1);
 
-
     //-------------- LIGHTING--------------
     // scene.background = new THREE.Color(0xdddddd)
 
     let hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 0);
     scene.add(hemiLight);
 
-    let directionalLightFront = new THREE.DirectionalLight(new THREE.Color('hsl(0, 0%, 100%)'), 2);
+    let directionalLightFront = new THREE.DirectionalLight(
+      new THREE.Color("hsl(0, 0%, 100%)"),
+      2
+    );
     directionalLightFront.position.set(0, 2, 5);
-    scene.add(directionalLightFront)
+    scene.add(directionalLightFront);
 
-    let directionalLightLeft = new THREE.DirectionalLight(new THREE.Color('hsl(0, 0%, 100%)'));
+    let directionalLightLeft = new THREE.DirectionalLight(
+      new THREE.Color("hsl(0, 0%, 100%)")
+    );
     directionalLightLeft.position.set(10, 5, -10);
-    scene.add(directionalLightLeft)
+    scene.add(directionalLightLeft);
 
-    let directionalLightRight = new THREE.DirectionalLight(new THREE.Color('hsl(0, 0%, 100%)'));
+    let directionalLightRight = new THREE.DirectionalLight(
+      new THREE.Color("hsl(0, 0%, 100%)")
+    );
     directionalLightRight.position.set(-10, 5, -10);
-    scene.add(directionalLightRight)
+    scene.add(directionalLightRight);
 
-    let pointLight = new THREE.PointLight(0xffffff, 100)
-    pointLight.position.set(0.5, 1, 2)
+    let pointLight = new THREE.PointLight(0xffffff, 100);
+    pointLight.position.set(0.5, 1, 2);
 
     const spotLight = new THREE.SpotLight(0xffffff, 3, 50, 26);
-    spotLight.position.set(0, 4, 0)
+    spotLight.position.set(0, 4, 0);
     scene.add(spotLight);
     // const spotHelper = new THREE.SpotLightHelper(spotLight)
     // scene.add(spotHelper);
     //-------------------------------------------
-
 
     // ENVIRONMENT
     // const env = new RGBELoader().load('models/studio_small_08_1k.hdr');
@@ -67,7 +74,6 @@ export default function Home() {
 
     // Assign Ref property for later cleanup
     const cleanup = mountRef.current;
-
 
     // FLOOR TEXTURE
     // -------Floor is currently disabled-------
@@ -92,25 +98,24 @@ export default function Home() {
 
     // LOAD OBJECT
     const loader = new GLTFLoader();
-    loader.load('models/del.glb', (gltf) => {
+    loader.load("models/del.glb", (gltf) => {
       let model = gltf.scene;
 
       // On load, animate camera position
       gsap.to(camera.position, {
         z: 3,
         duration: 1.5,
-        ease: 'power1.out',
-      })
+        ease: "power1.out",
+      });
       scene.add(model);
-    })
-
+    });
 
     //-------------- CAMERA CONTROLS---------------------
     const controls = new OrbitControls(camera, renderer.domElement);
     // Lock all controls
-    controls.enableZoom = false
-    controls.enablePan = false
-    controls.enableRotate = false
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.enableRotate = false;
     // Set camera position, looking down -z axis
     // camera.position.z = 3;
     controls.minPolarAngle = Math.PI / 2;
@@ -126,9 +131,8 @@ export default function Home() {
       requestAnimationFrame(animate);
 
       renderer.render(scene, camera);
-    }
+    };
     animate();
-
 
     // Remove scene on unmount
     return () => cleanup.removeChild(renderer.domElement);
@@ -137,12 +141,9 @@ export default function Home() {
   return (
     <div className="view-one">
       <div ref={mountRef}>
-        <h1 className="title">
-              Script.ly
-        </h1>
+        <h1 className="title">Script.ly</h1>
         <button className="button">Explore</button>
       </div>
     </div>
-  )
+  );
 }
-
