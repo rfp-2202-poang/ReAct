007ce58276944badf818d98ba3b4c2725b1ca5ec
Minggui Yactayo <minggui.yactayo@gmail.com>
2022-05-09 12:32:53 -0700

    RecordButtonStyling

--- components/homePage/UploadButton.js
+++ components/homePage/UploadButton.js
@@ -4,6 +4,27 @@ import styles from '../../styles/UploadButton.module.css'
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
+// const FORM_STYLES = {
+
+//     border: 'none',
+//     borderRadius: '20px',
+//     backgroundColor: 'rgba(176, 144, 61, 0.2)',
+//     opacity: '70%',
+//     transition: '1s',
+//     position: 'absolute',
+//     width: '429px',
+//     height: '109px',
+//     left: '557px',
+//     top: '448px',
+//     fontFamily: 'Inter',
+//     fontStyle: 'normal',
+//     fontWeight: '700',
+//     fontSize: '40px',
+//     lineHeight: '48px',
+//     textAlign: 'center',
+//     color: '#B0903D'
+// }
+
 const UploadButton = () => {
 
   const { register, handleSubmit } = useForm();
