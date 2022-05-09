7e2a3ebb280ba8ecb8359fb8d26cef236d121758
JoeVanCamp <vancampjoe@gmail.com>
2022-05-08 22:42:33 -0700

    Added "Learn More" button and "About" section with placeholders

---
+++ components/About.js
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from 'react';
+
+
+const About = () => {
+
+  return (
+    <>
+    <div className="container">
+      <h3 className="about">Speak with confidence. Speak with Script.ly.</h3>
+      <ul>
+        <li>Discover what message you're sending with Speech-Tone Analysis by IBM Watson.</li>
+        <li>Captivate your audience with Live Performance Practice.</li>
+        <li>Record scripts on the fly with Voice-2-Script.</li>
+        <li>Revise and edit your script with ease, even after upload.</li>
+      </ul>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default About;
